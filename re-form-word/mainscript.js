e9475c74b1a3cb3453c0f66fa1ef4ec2b159767f
nbur4556 <nbur4556@gmail.com>
2020-07-30 16:20:43 -0500

    completed reform word challenge

--- re-form-word/mainscript.js
+++ re-form-word/mainscript.js
@@ -1,1 +1,17 @@
-alert("Hello World");
+var firstHalfWord = prompt("Enter first half of a word, in lower case");
+var secondHalfWord = prompt("Enter second half of a word");
+
+var fullWordLowercase = firstHalfWord + secondHalfWord;
+var fullWordUppercase;
+
+for (let i = 0; i < fullWordLowercase.length; i++) {
+    console.log(fullWordLowercase[i]);
+    if (i == 0) {
+        fullWordUppercase = fullWordLowercase[i].toUpperCase();
+    }
+    else {
+        fullWordUppercase += fullWordLowercase[i].toLowerCase();
+    }
+}
+
+alert("Your word is " + fullWordUppercase);
