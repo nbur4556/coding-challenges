1ff82471e07dd801dbbc7e6fb0b86e9473c28ef2
nbur4556 <nbur4556@gmail.com>
2020-07-30 16:27:59 -0500

    included instructions, and noted javascript file

---
+++ re-form-word/INSTRUCTIONS.md
@@ -0,0 +1,16 @@
+# Re-Form Word Challenge
+
+Take a word that has been into 2 parts. Re-form the word by adding both halves together, changing the first character to an uppercase letter.
+
+Difficulty: Very Easy
+
+## Examples
+
+* "seas" + "onal" = "Seasonal"
+* "comp" + "lete" = "Complete"
+* "lang" + "uage" = "Language"
+
+## Resources
+
+Coding challenge from [edabit.com](edabit.com)
+[Challenge](https://edabit.com/challenge/KQQ7RMXjSK257GQgy)
--- re-form-word/mainscript.js
+++ re-form-word/mainscript.js
@@ -1,15 +1,18 @@
+// Get input from user
 var firstHalfWord = prompt("Enter first half of a word, in lower case");
 var secondHalfWord = prompt("Enter second half of a word");
 
 var fullWordLowercase = firstHalfWord + secondHalfWord;
 var fullWordUppercase;
 
+// Go through each letter in fullWordLowercase, and add to fullWordUppercase
 for (let i = 0; i < fullWordLowercase.length; i++) {
-    console.log(fullWordLowercase[i]);
     if (i == 0) {
+        // Capitolize the first letter
         fullWordUppercase = fullWordLowercase[i].toUpperCase();
     }
     else {
+        // All other letters lowercase
         fullWordUppercase += fullWordLowercase[i].toLowerCase();
     }
 }
